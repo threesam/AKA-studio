7cb9b4644601eec4e3f39aa346d6d1033098b551
threesam <salvatoredangelo@protonmail.com>
2021-04-28 14:21:29 -0400

    removed create and delete from page

--- schemas/documents/page.js
+++ schemas/documents/page.js
@@ -2,6 +2,7 @@ export default {
   name: 'page',
   type: 'document',
   title: 'Page',
+  __experimental_actions: [/*'create',*/ 'update', /*'delete',*/ 'publish'],
   fields: [
     {
       name: 'title',
